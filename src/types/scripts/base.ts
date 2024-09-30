9728434efa3ec55b3d31b66eb7e6621dd5c6e64e
jinyang628 <chenjinyang4192@gmail.com>
2024-09-29 17:10:38 -0700

    Update

--- src/types/scripts/base.ts
+++ src/types/scripts/base.ts
@@ -1,19 +1,20 @@
 import z from 'zod'
-import { backgroundScriptsEnumSchema } from "@/types/background";
-
+import { backgroundScriptsEnumSchema } from '@/types/background'
 
 export const navigateToUrlSchema = z.object({
-    messageType: backgroundScriptsEnumSchema,
-    url: z.string(),
+  messageType: backgroundScriptsEnumSchema,
+  url: z.string(),
 })
 
 export const clickButtonSchema = z.object({
-    messageType: backgroundScriptsEnumSchema,
-    classQuery: z.string(),
+  messageType: backgroundScriptsEnumSchema,
+  classQuery: z.string(),
 })
 
-export const fillInputSchema = z.object({
-    messageType: backgroundScriptsEnumSchema,
-    classQuery: z.string(),
-    value: z.string(),
+export const fillInputRequestSchema = z.object({
+  messageType: backgroundScriptsEnumSchema,
+  id: z.string(),
+  value: z.string(),
 })
+
+export type FillInputRequest = z.infer<typeof fillInputRequestSchema>
