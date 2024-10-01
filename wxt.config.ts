452270a433c84ff001cc3e8afe6df7dc01533f02
Aarontanzb <aarontanzb@gmail.com>
2024-09-30 20:52:24 -0700

    host permissions

--- wxt.config.ts
+++ wxt.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
     description: "__MSG_extension_description__",
     name: "__MSG_extension_name__",
     permissions: ["tabs", "activeTab"],
+    host_permissions: ["https://console.cloud.google.com/*", "http://localhost:3000/*"],
   },
   modules: ["@wxt-dev/module-react", "@wxt-dev/i18n/module"],
   srcDir: "src",
