9a47343fcb847120ceaf5c6d6dd5ffc547790d5a
jinyang628 <chenjinyang4192@gmail.com>
2024-10-12 14:52:12 -0700

    Update

--- src/entrypoints/injected.ts
+++ src/entrypoints/injected.ts
@@ -5,26 +5,22 @@ import {
   createGoogleOauth2ApplicationPartFour,
   createGoogleOauth2ApplicationPartTwo,
   createGoogleOauth2ApplicationPartOne,
-} from "@/scripts/injected/google";
-import { createLinearOauth2ApplicationPartOne } from "@/scripts/injected/linear";
-import { createSlackOauth2ApplicationPartOne } from "@/scripts/injected/slack";
-import { createXOauth2ApplicationPartOne } from "@/scripts/injected/x";
-import { createRedditOauth2ApplicationPartOne } from "@/scripts/injected/reddit";
-import { MessageTypeEnum, messageTypeEnumSchema } from "@/types/message";
+} from "@/scripts/injected/integrations/google";
+import { createLinearOauth2ApplicationPartOne } from "@/scripts/injected/integrations/linear";
+import { createSlackOauth2ApplicationPartOne } from "@/scripts/injected/integrations/slack";
+import { createXOauth2ApplicationPartOne } from "@/scripts/injected/integrations/x";
+import { createRedditOauth2ApplicationPartOne } from "@/scripts/injected/integrations/reddit";
+import { messageTypeEnumSchema } from "@/types/message";
 import { PlatformDetails } from "@/types/platform";
 import {
-  BaseRequest,
-  clickRequestSchema,
-  fillInputRequestSchema,
   InjectButtonRequest,
   injectButtonRequestSchema,
   navigationStateEnumSchema,
   querySelectorSchema,
-  RetrieveRequest,
-  retrieveRequestSchema,
 } from "@/types/scripts/base";
-import { createHubspotOauth2ApplicationPartOne } from "@/scripts/injected/hubspot";
+import { createHubspotOauth2ApplicationPartOne } from "@/scripts/injected/integrations/hubspot";
 import { logoBase64 } from "@/constants/logoBase64";
+import { waitUntilActionMessageResolved, waitUntilPageLoaded, waitUntilRetrieveMessageResolved } from "@/scripts/injected/utils";
 
 const GOOGLE_CLOUD_BASE_URL = "https://console.cloud.google.com";
 const GOOGLE_CLOUD_START_PAGE_BASE_URL: string =
@@ -121,90 +117,6 @@ const createButton = (autoClick: boolean, onClick: () => Promise<void>) => {
     button.click();
   }
 };
-
-// We probably do not want to unify this helper functions into one because the messages' schema overlap with one another and safeParse might lead to the wrong condition. Additionally, these scripts need to be passed into the function, and cannot be imported
-async function waitUntilActionMessageResolved(
-  request: BaseRequest,
-): Promise<void> {
-  let requestInstance: BaseRequest;
-  let responseMessageType: MessageTypeEnum;
-
-  if (request.type === messageTypeEnumSchema.Values.click) {
-    if (clickRequestSchema.safeParse(request).success) {
-      console.log("Waiting for Click Message to be resolved");
-      requestInstance = clickRequestSchema.parse(request);
-      responseMessageType = messageTypeEnumSchema.Values.clickResponse;
-      updateButtonText(navigationStateEnumSchema.Values.click);
-    } else {
-      throw new Error("Invalid request type for click");
-    }
-  } else if (request.type === messageTypeEnumSchema.Values.fillInput) {
-    console.log("Waiting for Fill Input Message to be resolved");
-    if (fillInputRequestSchema.safeParse(request).success) {
-      requestInstance = fillInputRequestSchema.parse(request);
-      responseMessageType = messageTypeEnumSchema.Values.fillInputResponse;
-      updateButtonText(navigationStateEnumSchema.Values.fill);
-    } else {
-      throw new Error("Invalid request type for fillInput");
-    }
-  }
-
-  await new Promise<void>((resolve) => {
-    const interval = setInterval(() => {
-      window.postMessage(requestInstance, "*");
-    }, 1000);
-
-    const listener = (event: any) => {
-      if (event.source !== window) return;
-      if (event.data.type === responseMessageType) {
-        clearInterval(interval);
-        window.removeEventListener("message", listener);
-        console.log("Message received:", event.data);
-        resolve(event.data.value);
-      }
-    };
-    window.addEventListener("message", listener);
-  });
-}
-
-async function waitUntilRetrieveMessageResolved(
-  request: RetrieveRequest,
-): Promise<string> {
-  console.log("Waiting for Retrieve Message to be resolved");
-  let requestInstance: RetrieveRequest;
-  let responseMessageType: MessageTypeEnum;
-
-  if (retrieveRequestSchema.safeParse(request).success) {
-    requestInstance = retrieveRequestSchema.parse(request);
-    responseMessageType = messageTypeEnumSchema.Values.retrieveResponse;
-    updateButtonText(navigationStateEnumSchema.Values.retrieve);
-  } else {
-    throw new Error("Invalid request type");
-  }
-
-  return new Promise<string>((resolve) => {
-    const interval = setInterval(() => {
-      window.postMessage(requestInstance, "*");
-    }, 1000);
-
-    const listener = (event: MessageEvent) => {
-      if (event.source !== window) return;
-      if (event.data.type === responseMessageType) {
-        clearInterval(interval);
-        window.removeEventListener("message", listener);
-        resolve(event.data.value);
-      }
-    };
-    window.addEventListener("message", listener);
-  });
-}
-
-async function waitUntilPageLoaded() {
-  updateButtonText(navigationStateEnumSchema.Values.wait);
-  await new Promise((resolve) => {
-    window.addEventListener("load", resolve);
-  });
-}
 
 async function injectButton({
   autoClick,
